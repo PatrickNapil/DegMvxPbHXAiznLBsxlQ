a5760cdbff5e915cfc2cb89f973b8b6134d9d89d
Rey Kristian Cabanilla <rkvc95@yahoo.com.ph>
2016-04-03 19:23:04 +0800

    Added flying enemy that drops rock

---
+++ Project Platformer/Assets/Scripts/FlyingEnemyB.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingEnemyB : MonoBehaviour {
+
+	public float attackDelay = 1f;
+	public float aimDelay = 1f;
+	private Animator animator;
+	public OrangeRock projectile;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+		StartCoroutine(OnAttack());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator OnAttack() {
+		yield return new WaitForSeconds (aimDelay);
+		Aim ();
+		yield return new WaitForSeconds (attackDelay);
+		Fire ();
+		StartCoroutine (OnAttack ());
+	}
+
+	void Fire() {
+		animator.SetInteger ("AnimState", 1);
+	}
+
+	void Aim() {
+		animator.SetInteger ("AnimState", 0);
+	}
+
+	void OnShoot() {
+		if (projectile) {
+			OrangeRock falling_rock = Instantiate (projectile, transform.position,
+				                          Quaternion.identity) as OrangeRock;
+		}
+	}
+}
